26327341d5394689f3db6ce053c40f1fbf0a1702
MarcSogas <marc.sogas@gmail.com>
2022-11-05 11:24:16 +0100

    data providder

---
+++ app/src/main/java/com/marcsogasdev/exemplemvvm/model/QuoteProvider.kt
@@ -0,0 +1,53 @@
+package com.marcsogasdev.exemplemvvm.model
+
+//this provider emulate Retrofit or Room
+
+class QuoteProvider {
+    private companion object {
+
+        fun random():QuoteModel{
+            //random number 0 to 10
+            val position = (0..10).random()
+            return quote[position]
+        }
+
+        //list of quote. The user change the quote when push the screen
+        val quote = listOf<QuoteModel>(
+            QuoteModel(
+                quote = "It’s not a bug. It’s an undocumented feature!",
+                author = "Anonymous"
+            ),
+            QuoteModel(
+                quote = "“Software Developer” – An organism that turns caffeine into software",
+                author = "Anonymous"
+            ),
+            QuoteModel(
+                quote = "If debugging is the process of removing software bugs, then programming must be the process of putting them in",
+                author = "Edsger Dijkstra"
+            ),
+            QuoteModel(
+                quote = "A user interface is like a joke. If you have to explain it, it’s not that good.",
+                author = "Anonymous"
+            ),
+            QuoteModel(
+                quote = "I don’t care if it works on your machine! We are not shipping your machine!",
+                author = "Vidiu Platon"
+            ),
+            QuoteModel(
+                quote = "Measuring programming progress by lines of code is like measuring aircraft building progress by weight.",
+                author = "Bill Gates"
+            ),
+            QuoteModel(
+                quote = "My code DOESN’T work, I have no idea why. My code WORKS, I have no idea why.",
+                author = "Anonymous"
+            ),
+            QuoteModel(quote = "Things aren’t always #000000 and #FFFFFF", author = "Anonymous"),
+            QuoteModel(quote = "Talk is cheap. Show me the code.", author = "Linus Torvalds"),
+            QuoteModel(
+                quote = "Software and cathedrals are much the same — first we build them, then we pray.",
+                author = "Anonymous"
+            ),
+            QuoteModel(quote = "¿A que esperas?, contratame.", author = "Marc Sogas 😊")
+        )
+    }
+}
